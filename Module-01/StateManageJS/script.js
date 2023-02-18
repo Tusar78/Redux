a0ec6260c3ebfd753dc88ad5d77c492cab4230a8
Tusar <tusarahmed141@gmail.com>
2023-02-18 11:38:58 +0600

    Maximum productivce way to change state

--- Module-01/StateManageJS/script.js
+++ Module-01/StateManageJS/script.js
@@ -2,18 +2,21 @@
 const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
+const counter2El = document.getElementById("counter2");
+const increment2El = document.getElementById("increment2");
+const decrement2El = document.getElementById("decrement2");
 
 // Initialize Counter value
 let count = 0;
 
-const counterEvent = ({ elem, text }) => {
+const counterEvent = ({ elem, text, ui }) => {
   elem.addEventListener("click", () => {
     if (text === "+") {
       count++;
-      counterEl.innerText = count;
+      ui.innerText = count;
     } else if (text === "-") {
       count--;
-      counterEl.innerText = count;
+      ui.innerText = count;
     } else {
       alert("Please valid Parameter");
     }
@@ -23,12 +26,27 @@ const counterEvent = ({ elem, text }) => {
 const increment = {
   elem: incrementEl,
   text: "+",
+  ui: counterEl
 };
 
 const decrement = {
   elem: decrementEl,
   text: "-",
+  ui: counterEl
+};
+const increment2 = {
+  elem: increment2El,
+  text: "+",
+  ui: counter2El
+};
+
+const decrement2 = {
+  elem: decrement2El,
+  text: "-",
+  ui: counter2El
 };
 
 counterEvent(increment);
 counterEvent(decrement);
+counterEvent(increment2);
+counterEvent(decrement2);
