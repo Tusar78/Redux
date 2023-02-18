845185d3c1478c6a42dda46ee31e13fa83d4fe91
Tusar <tusarahmed141@gmail.com>
2023-02-18 16:49:58 +0600

    Created new component called stats

--- Module-01/counter/src/App.js
+++ Module-01/counter/src/App.js
@@ -1,16 +1,17 @@
 import Counter from "./Components/Counter";
+import Stats from "./Components/Stats";
 
 function App() {
   return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+    <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
         Simple Counter Application
       </h1>
 
-      <div class="max-w-md mx-auto mt-10 space-y-5">
+      <div className="max-w-md mx-auto mt-10 space-y-5">
         <Counter />
         <Counter />
-        <Counter />
+        <Stats count={2} />
       </div>
     </div>
   );
