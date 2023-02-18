d896f4cb6df22bfb0874f87fc513f11d51d8c86c
Tusar <tusarahmed141@gmail.com>
2023-02-18 17:48:38 +0600

    Module one last update

--- Module-01/counter/src/App.js
+++ Module-01/counter/src/App.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import Counter from "./Components/Counter";
 import Stats from "./Components/Stats";
 
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  {
+    id: 2,
+    count: 0,
+  },
+];
+
 function App() {
+  const [state, setState] = useState(initialState);
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0);
+  }
+  console.log(totalCount());
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
       <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
@@ -11,7 +28,7 @@ function App() {
       <div className="max-w-md mx-auto mt-10 space-y-5">
         <Counter />
         <Counter />
-        <Stats count={2} />
+        <Stats count={totalCount()} />
       </div>
     </div>
   );
