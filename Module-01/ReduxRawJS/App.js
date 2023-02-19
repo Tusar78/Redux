b7bb1bc25385c2030e613e2a161017e93f5897c5
Tusar <tusarahmed141@gmail.com>
2023-02-19 11:11:54 +0600

    Action creation done

--- Module-01/ReduxRawJS/App.js
+++ Module-01/ReduxRawJS/App.js
@@ -3,8 +3,65 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
-// 
+// Dispatch Identifier
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
 
+// Initialize Count 
+const initializeValue = {
+  value: 0,
+}
+
+const increment = () => {
+  return {
+    type: INCREMENT,
+    payload: 5
+  }
+}
+
+const decrement = () => {
+  return {
+    type: DECREMENT,
+    payload: 5
+  }
+}
+
+// Create Redux Reducer
+const createReducer = (state = initializeValue, {type, payload}) => {
+  if (type === 'increment') {
+    return {
+      ...state,
+      value: state.value + payload
+    }
+  } else if (type === 'decrement') {
+    return {
+      ...state,
+      value: state.value - payload
+    }
+  } else {
+    return state;
+  }
+}
+
+// Create Redux store
+const store = Redux.createStore(createReducer);
+
+// Action dispatch
+incrementEl.addEventListener('click', () => {
+  store.dispatch(increment())
+})
+
+decrementEl.addEventListener('click', () => {
+  store.dispatch(decrement())
+})
+
+// Store subscribe
+const render = () => {
+  const state = store.getState();
+  counterEl.innerText = state.value;
+}
+
+store.subscribe(render)
 
 // // Initialize Value
 // const initializeValue = {
