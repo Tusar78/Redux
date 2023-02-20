6c1070229f7b3d4d173de53e29d3be80d32c5289
Tusar <tusarahmed141@gmail.com>
2023-02-20 18:48:54 +0600

    Dom children feature explore

--- Module-01/DOM/app.js
+++ Module-01/DOM/app.js
@@ -1,7 +1,7 @@
 const todoTitle = document.getElementById('todo-title');
 
 const items = document.getElementById('items');
-console.log(items);
+// console.log(items);
 // const listItem = items.getElementsByClassName('item')
 // for (let i = 0; i < listItem.length; i++) {
 //   const element = listItem[i];
@@ -18,3 +18,8 @@ console.log(items);
 // const lastItem = document.querySelector('#items').querySelector('.item:nth-child(2)');
 // console.log(lastItem);
 // lastItem.style.color = 'red';
+
+const grandParent = document.querySelector('.incomplete-task');
+const parent = grandParent.children;
+const item = parent[1].children;
+console.log(item);
