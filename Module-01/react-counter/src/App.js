902b7884e3e7b40cb0abf28be9834cdf6b9ca633
Tusar <tusarahmed141@gmail.com>
2023-02-22 01:14:13 +0600

    Last update

--- Module-01/react-counter/src/App.js
+++ Module-01/react-counter/src/App.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import Counter from './Components/Counter';
+import React from "react";
+import Counter from "./Components/Counter";
 
 const App = () => {
   return (
     <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-    <h1 class="max-w-md mx-auto text-center text-2xl font-bold todo-title">
-      Simple Counter Application
-    </h1>
-    <div class="max-w-md mx-auto mt-10 space-y-5">
-      <div
-        class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-      >
-        <div class="text-2xl font-semibold" id="counter">0</div>
-        <Counter />
-      </div>
+      <h1 class="max-w-md mx-auto text-center text-2xl font-bold todo-title">
+        Simple Counter Application
+      </h1>
+      <Counter />
     </div>
-  </div>
   );
 };
 
--- Module-01/react-counter/src/Components/Counter.js
+++ Module-01/react-counter/src/Components/Counter.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const increment = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+  const decrement = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
   return (
-    <div class="flex space-x-3">
-      <button
-        class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-        id="increment"
-      >
-        Increment
-      </button>
-      <button
-        class="bg-red-400 text-white px-3 py-2 rounded shadow"
-        id="decrement"
-      >
-        Decrement
-      </button>
-    </div>
+    <div class="max-w-md mx-auto mt-10 space-y-5">
+        <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+          <div class="text-2xl font-semibold" id="counter">
+            {counter}
+          </div>
+          <div class="flex space-x-3">
+            <button
+              class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+              onClick={increment}
+            >
+              Increment
+            </button>
+            <button
+              class="bg-red-400 text-white px-3 py-2 rounded shadow"
+              onClick={decrement}
+            >
+              Decrement
+            </button>
+          </div>
+        </div>
+      </div>
   );
 };
 
