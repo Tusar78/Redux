f4c4789e66c4f4885da06b7bc9aeffde46a9630a
Tusar <tusarahmed141@gmail.com>
2023-02-23 21:09:11 +0600

    mapStateToProps done

--- Module-02/react-redux/src/components/Counter.js
+++ Module-02/react-redux/src/components/Counter.js
@@ -1,19 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 import Button from "./Button";
 import Count from "./Count";
 
-const Counter = ({count, increment, decrement}) => {
+const Counter = ({count}) => {
   return (
     <div className="max-w-md mx-auto mt-10 space-y-5">
       <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
         <Count count={count} />
         <div className="flex space-x-3">
-          <Button handler={increment}>Increment</Button>
-          <Button handler={decrement}>Decrement</Button>
+          <Button>Increment</Button>
+          <Button>Decrement</Button>
         </div>
       </div>
     </div>
   );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    count: state.value
+  }
+}
+
+const mapDispatchToProps = () => {
+  return {
+    hi: 'hello'
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
