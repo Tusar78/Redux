704af787e99772cfb35e465e45cb256094ca2b31
Tusar <tusarahmed141@gmail.com>
2023-02-25 23:53:37 +0600

    Last update in connect api redux

--- Module-02/react-redux/src/components/Counter.js
+++ Module-02/react-redux/src/components/Counter.js
@@ -6,7 +6,9 @@ const Counter = ({ count, increment, decrement, id }) => {
   return (
     <div className="max-w-md mx-auto mt-10 space-y-5">
       <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-      <div className="text-2xl font-semibold" id="counter">{count}</div>
+        <div className="text-2xl font-semibold" id="counter">
+          {count}
+        </div>
         <div className="flex space-x-3">
           <button
             className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
@@ -25,6 +27,21 @@ const Counter = ({ count, increment, decrement, id }) => {
     </div>
   );
 };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state.value,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: (value) => dispatch(increment(value)),
+//     decrement: (value) => dispatch(decrement(value)),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 const mapStateToProps = (state) => {
   return {
